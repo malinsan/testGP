0af3ee398404aaec870047edb0edfafd6c7a6cce
Malin Thelin <malin.thelin12@gmail.com>
2018-09-11 09:33:06 +0200

    Function to generate random number between min and max

--- testGP/main.cpp
+++ testGP/main.cpp
@@ -32,6 +32,7 @@ extern "C" {
 
 #pragma GCC diagnostic ignored "-Wreturn-type"
 
+StringPrinter sp;
 
 void blink_led_init()
 {
@@ -40,11 +41,7 @@ void blink_led_init()
 
  RCC->AHB1ENR |= BLINK_RCC_MASKx(BLINK_PORT_NUMBER);
 
-
-
  GPIO_InitTypeDef GPIO_InitStructure;
-
-
 
  // Configure pin in output push/pull mode
 
@@ -66,11 +63,25 @@ void blink_led_init()
 
 }
 
+/**
+  Get a random number between min and max.
+  Uses functions from stm32f4xx_rng.c
+*/
+uint8_t getRandomNumber(uint32_t min, uint8_t max){
+  uint32_t random = 0;
+
+  while(random <= min){
+    if(RNG_GetFlagStatus(RNG_FLAG_DRDY)){
+      random = RNG_GetRandomNumber();
+      random &= max;
+    }
+  }
+  return (uint8_t)random;
+}
 
 
-int main(int argc, char* argv[])
 
-{
+int main(int argc, char* argv[]){
 
   SystemInit();
 
@@ -87,43 +98,37 @@ int main(int argc, char* argv[])
   timer_start();
 
 
-
-  StringPrinter sp;
-
   char t[10] = "Test";
   char yoMom[10] = "Din Mamma";
 
 
   char yo[10] = "yoooooo";
 
-  uint32_t a;
+  //random number generation
   RCC_AHB2PeriphClockCmd(RCC_AHB2Periph_RNG, ENABLE);
   RNG_Cmd(ENABLE);
-  if(RNG_GetFlagStatus(RNG_FLAG_DRDY)){
-    a = RNG_GetRandomNumber();
-  }
-  char random[5];
-  sprintf(random,"%d", a);
+
 
   sp.printStartUp();
 
+   while(1){
 
-   while(1)
+     uint8_t a = getRandomNumber(5,30);
+     char random[5];
+     sprintf(random,"%d", a);
 
-   {
+      blink_led_on();
 
-    blink_led_on();
-
-    timer_sleep(BLINK_ON_TICKS);
+      timer_sleep(BLINK_ON_TICKS);
 
 
 
-    blink_led_off();
+      blink_led_off();
 
-    sp.printText(yoMom);
-    sp.printText(random);
+      sp.printText(yoMom);
+      sp.printText(random);
 
-    timer_sleep(BLINK_OFF_TICKS);
+      timer_sleep(BLINK_OFF_TICKS);
 
    }
 
